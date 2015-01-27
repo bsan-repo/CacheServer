55c517dc2624f2e9b3e8ca3bb015738b4877818a
Boris Sanchez <boris.snchz@gmail.com>
2015-01-27 23:02:51 +1000

    Initial project set up, database model definition

---
+++ app/database/migrations/2015_01_27_115553_create_citing_works_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitingWorksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('citing_works', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('authors');
+			$table->string('publisher');
+			$table->string('rankPublisher');
+			$table->integer('authorwork_id')->unsigned();
+			$table->foreign('authorwork_id')->references('id')->on('author_works')->onDelete("cascade");
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('citing_works');
+	}
+
+}
